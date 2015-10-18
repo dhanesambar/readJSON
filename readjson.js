b02ef189bf04162e7e72193ade61874c69df90a5
Ambar Dhaneswara <ambardhanes@gmail.com>
2015-10-18 13:19:49 +0700

    memperbaiki file json

--- readjson.js
+++ readjson.js
@@ -2,9 +2,17 @@ var fs = require('fs');
 
 fs.readFile(__dirname + '/news.json', function(err, data) {
     var jsonObj = JSON.parse(data);
-    console.log("Headline News = " + jsonObj.Headline);
-    console.log("Body News = " + jsonObj.Body);
-    console.log("Post Date = " + jsonObj.posted_on);
-    console.log("Post By = " + jsonObj.posted_by);
+    console.log("Headline News = " + jsonObj[0].headlines);
+    console.log("Body News = " + jsonObj[0].Body);
+    console.log("Post Date = " + jsonObj[0].posted_on);
+    console.log("Post By = " + jsonObj[0].posted_by);
 
+
+    var jumrec = jsonObj.length;
+    for (var i = 0; i<jumrec; i++) {
+      console.log("Headline News = " + jsonObj[i].headlines);
+      console.log("Body News = " + jsonObj[i].Body);
+      console.log("Post Date = " + jsonObj[i].posted_on);
+      console.log("Post By = " + jsonObj[i].posted_by);
+    }
 });
